55cd8d55e3b33f68cf569333c289c091006c676b
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-05-12 15:02:51 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() 
+
+   {
+  let GeneradorExcusa = () => 
+    {
+       let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+       let who2 = who [Math.floor(Math.random()*(who.length))];
+
+       let action = ['ate', 'peed', 'crushed', 'broke'];
+       let action2 = action[Math.floor(Math.random()*(action.length))];
+
+       let what = ['my homework', 'my phone', 'the car'];
+       let what2 = what [Math.floor(Math.random()*(what.length))];
+
+       let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+       let when2 = when [Math.floor(Math.random()*(when.length))];
+
+       let GeneradorExcusa = `${who2} ${action2} ${what2} ${when2}`;
+       return GeneradorExcusa
+
+  }
+
+document.getElementById("excuse").innerHTML = GeneradorExcusa()
+}
+
+   
+ 
